bca95eb8f0a49ef426921d9c8765a80fc4728dbc
Lohitha_Mahesh <63092393+lohitha-17@users.noreply.github.com>
2026-01-29 15:30:03 -0600

    Add revenue_daily.sql

--- sql/marts/revenue_daily.sql
+++ sql/marts/revenue_daily.sql
@@ -1,1 +1,17 @@
 
+-- Daily revenue from completed orders
+SELECT
+  DATE(o.created_at) AS order_date,
+  SUM(oi.sale_price) AS daily_revenue
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+JOIN
+  `bigquery-public-data.thelook_ecommerce.orders` o
+ON
+  oi.order_id = o.order_id
+WHERE
+  o.status = 'Complete'
+GROUP BY
+  DATE(o.created_at)
+ORDER BY
+  order_date;
