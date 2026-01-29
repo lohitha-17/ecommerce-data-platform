1f9f5327c9a83e61f234144661e56fff40e89dcf
Lohitha_Mahesh <63092393+lohitha-17@users.noreply.github.com>
2026-01-29 15:32:39 -0600

    Add revenue_monthly.sql

--- sql/marts/revenue_monthly.sql
+++ sql/marts/revenue_monthly.sql
@@ -1,1 +1,17 @@
+-- Monthly revenue from completed orders
+SELECT
+  FORMAT_DATE('%Y-%m', DATE(o.created_at)) AS year_month,
+  SUM(oi.sale_price) AS monthly_revenue
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+JOIN
+  `bigquery-public-data.thelook_ecommerce.orders` o
+ON
+  oi.order_id = o.order_id
+WHERE
+  o.status = 'Complete'
+GROUP BY
+  year_month
+ORDER BY
+  year_month;
 
